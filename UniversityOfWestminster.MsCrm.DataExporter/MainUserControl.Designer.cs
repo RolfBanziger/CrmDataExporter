07f4b91198cfc7a0700dfbf2cf6f3db58e6d7419
Rolf <Rolf@BRBX1>
2016-11-05 19:11:07 +0000

    Add project files.

---
+++ UniversityOfWestminster.MsCrm.DataExporter/MainUserControl.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace UniversityOfWestminster.MsCrm.DataExporter
+{
+    partial class MainUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainUserControl));
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbGo = new System.Windows.Forms.ToolStripButton();
+            this.saveFileDialogCSV = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialogXML = new System.Windows.Forms.SaveFileDialog();
+            this.pageDatatable = new System.Windows.Forms.TabPage();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.pageRequest = new System.Windows.Forms.TabPage();
+            this.txtRequest = new System.Windows.Forms.RichTextBox();
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tsbSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripMenu.SuspendLayout();
+            this.pageDatatable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.pageRequest.SuspendLayout();
+            this.tcMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.toolStripSeparator2,
+            this.tsbGo,
+            this.tsbSave});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripMenu.Size = new System.Drawing.Size(522, 27);
+            this.toolStripMenu.TabIndex = 5;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbClose.Image = ((System.Drawing.Image)(resources.GetObject("tsbClose.Image")));
+            this.tsbClose.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(24, 24);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // tsbGo
+            // 
+            this.tsbGo.Image = ((System.Drawing.Image)(resources.GetObject("tsbGo.Image")));
+            this.tsbGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbGo.Name = "tsbGo";
+            this.tsbGo.Size = new System.Drawing.Size(71, 24);
+            this.tsbGo.Text = "Execute";
+            this.tsbGo.ToolTipText = "Perfomrs a Who I Am request";
+            this.tsbGo.Click += new System.EventHandler(this.tsbWhoAmI_Click);
+            // 
+            // saveFileDialogCSV
+            // 
+            this.saveFileDialogCSV.DefaultExt = "csv";
+            this.saveFileDialogCSV.Filter = "CSV files|*.csv|All files|*.*";
+            // 
+            // saveFileDialogXML
+            // 
+            this.saveFileDialogXML.DefaultExt = "xml";
+            this.saveFileDialogXML.Filter = "XML files|*.xml|All files|*.*";
+            this.saveFileDialogXML.Title = "Save result set";
+            // 
+            // pageDatatable
+            // 
+            this.pageDatatable.Controls.Add(this.grid);
+            this.pageDatatable.Location = new System.Drawing.Point(4, 22);
+            this.pageDatatable.Name = "pageDatatable";
+            this.pageDatatable.Padding = new System.Windows.Forms.Padding(3);
+            this.pageDatatable.Size = new System.Drawing.Size(514, 329);
+            this.pageDatatable.TabIndex = 2;
+            this.pageDatatable.Text = "Data";
+            this.pageDatatable.UseVisualStyleBackColor = true;
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToOrderColumns = true;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(3, 3);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.Size = new System.Drawing.Size(508, 323);
+            this.grid.TabIndex = 0;
+            // 
+            // pageRequest
+            // 
+            this.pageRequest.Controls.Add(this.txtRequest);
+            this.pageRequest.Location = new System.Drawing.Point(4, 22);
+            this.pageRequest.Name = "pageRequest";
+            this.pageRequest.Padding = new System.Windows.Forms.Padding(3);
+            this.pageRequest.Size = new System.Drawing.Size(514, 329);
+            this.pageRequest.TabIndex = 0;
+            this.pageRequest.Text = "Request";
+            this.pageRequest.UseVisualStyleBackColor = true;
+            // 
+            // txtRequest
+            // 
+            this.txtRequest.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtRequest.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.txtRequest.Location = new System.Drawing.Point(3, 3);
+            this.txtRequest.Name = "txtRequest";
+            this.txtRequest.Size = new System.Drawing.Size(508, 323);
+            this.txtRequest.TabIndex = 0;
+            this.txtRequest.Text = resources.GetString("txtRequest.Text");
+            // 
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.pageRequest);
+            this.tcMain.Controls.Add(this.pageDatatable);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 27);
+            this.tcMain.Multiline = true;
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(522, 355);
+            this.tcMain.TabIndex = 6;
+            // 
+            // tsbSave
+            // 
+            this.tsbSave.Image = ((System.Drawing.Image)(resources.GetObject("tsbSave.Image")));
+            this.tsbSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSave.Name = "tsbSave";
+            this.tsbSave.Size = new System.Drawing.Size(55, 24);
+            this.tsbSave.Text = "Save";
+            this.tsbSave.Click += new System.EventHandler(this.asCSVToolStripMenuItem_Click);
+            // 
+            // MainUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tcMain);
+            this.Controls.Add(this.toolStripMenu);
+            this.Name = "MainUserControl";
+            this.Size = new System.Drawing.Size(522, 382);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.pageDatatable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.pageRequest.ResumeLayout(false);
+            this.tcMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton tsbGo;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogCSV;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogXML;
+        private System.Windows.Forms.ToolStripButton tsbSave;
+        private System.Windows.Forms.TabPage pageDatatable;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.TabPage pageRequest;
+        private System.Windows.Forms.RichTextBox txtRequest;
+        private System.Windows.Forms.TabControl tcMain;
+    }
+}
---
+++ UniversityOfWestminster.MsCrm.DataExporter/MainUserControl.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+using XrmToolBox.Extensibility.Args;
+using Microsoft.Crm.Sdk.Messages;
+using System.IO;
+using System.Xml;
+using Microsoft.Xrm.Sdk;
+
+namespace UniversityOfWestminster.MsCrm.DataExporter
+{
+    public partial class MainUserControl : PluginControlBase, IStatusBarMessenger
+    {
+        public MainUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private string Message { get; set; }
+
+        public event EventHandler<StatusBarMessageEventArgs> SendMessageToStatusBar;
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void ProcessFetchXmlQuery(string query)
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Fetching data...",
+                AsyncArgument = query,
+                IsCancelable = true,
+
+                Work = (bw, e) =>
+                {
+                var action = new FetchXmlAction(Service);
+                    action.MessageChanged += delegate (object sender, EventArgs eventArgs)
+                    {
+                        if (SendMessageToStatusBar != null)
+                        {
+                            SendMessageToStatusBar(this, new StatusBarMessageEventArgs(
+                                ((FetchXmlAction)sender).Message) );
+                        }
+                    };
+
+                    e.Result = action.RunFetchXmlQuery(e.Argument.ToString());
+
+                },
+                PostWorkCallBack = e =>
+                {
+                    if (e.Error == null)
+                    {
+                        ProcessResponse((IList<Entity>)e.Result);
+                    }
+                    else
+                    {
+                        MessageBox.Show(this, "An error occured: " + e.Error.Message, "Error", MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                    }
+                }
+            });
+
+        }
+        
+        private void ClearTheGrid()
+        {
+            grid.Rows.Clear();
+            grid.Columns.Clear();
+        }
+
+        private void ProcessResponse(IList<Entity> result)
+        {
+            ClearTheGrid();
+            if (result.Count == 0) return;
+            List<string> attributes = new List<string>();
+            foreach (var entity in result)
+            {
+                foreach (string a in entity.Attributes.Keys)
+                {
+                    if (!attributes.Contains(a))
+                    {
+                        attributes.Add(a);
+                        DataGridViewColumn column = new DataGridViewTextBoxColumn();
+                        column.Name = a;
+                        column.HeaderText = a;
+                        grid.Columns.Add(column);
+                    }
+                }
+            }
+            foreach (var entity in result)
+            {
+                grid.Rows.Add(EnumerateEntityValues(entity, attributes).ToArray());
+            }
+            tcMain.SelectedTab = pageDatatable;
+        }
+
+        private IEnumerable<string> EnumerateEntityValues(Entity entity, IEnumerable<string> attributes)
+        {
+            //foreach (string key in entity.Attributes.Keys)
+            foreach (string key in attributes)
+            {
+                if (!entity.Contains(key))
+                {
+                    yield return null;
+                    continue;
+                }
+                object value = entity[key];
+                yield return FormatObject(value);
+                
+            }
+        }
+
+        private string FormatObject(object value)
+        {
+            if (value is EntityReference)
+                return FormatObject((EntityReference)value);
+            else if (value is EntityCollection)
+                return FormatObject((EntityCollection)value);
+            else if (value is Guid)
+                return FormatObject((Guid)value);
+            else if (value is OptionSetValue)
+                return ((OptionSetValue)value).Value.ToString();
+            else if (value is Money)
+                return ((Money)value).Value.ToString();
+            else if (value is DateTime)
+                return ((DateTime)value).ToString("s");
+            else if (value is AliasedValue)
+                return FormatObject((AliasedValue)value);
+            else
+                return value.ToString();
+        }
+        
+
+        private string FormatObject(AliasedValue value)
+        {
+            return FormatObject(value.Value);
+        }
+
+        private string FormatObject(EntityCollection value)
+        {
+            StringBuilder formattedValue = new StringBuilder();
+            for (int i = 0; i< value.Entities.Count; i++)
+            {
+                var e = value.Entities[i];
+                formattedValue.Append(e.Id.ToString("B"));
+                if (i < value.Entities.Count-1)
+                {
+                    formattedValue.Append(", ");
+                }
+            }
+            return formattedValue.ToString();
+        }
+
+        private string FormatObject(Guid value)
+        {
+            return value.ToString("B");
+        }
+
+        private string FormatObject(EntityReference value)
+        {
+            if ((value.LogicalName == "systemuser")||(value.LogicalName == "team")) return value.Name;
+            else return FormatObject(value.Id);
+        }
+
+        private void tsbWhoAmI_Click(object sender, EventArgs e)
+        {
+            ProcessFetchXmlQuery(txtRequest.Text);
+        }
+
+        private void asCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialogCSV.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    SaveDGVtoCSV(saveFileDialogCSV.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void SaveDGVtoCSV(string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName,false, Encoding.Unicode))
+            {
+                // Header row
+                for (int idx = 0; idx < grid.ColumnCount; idx++)
+                {
+                    writer.Write(FormatCsvCellValue(grid.Columns[idx].Name));
+                    if (idx < grid.ColumnCount - 1)
+                    {
+                        writer.Write(',');
+                    }
+                }
+                writer.WriteLine();
+                // Data rows
+                foreach (DataGridViewRow row in grid.Rows)
+                {
+                    for (int idx = 0; idx < row.Cells.Count; idx++)
+                    {
+                        writer.Write(FormatCsvCellValue(row.Cells[idx].Value));
+                        if (idx < row.Cells.Count - 1)
+                        {
+                            writer.Write(',');
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Formats a value to be save to written in a CSV file.
+        /// </summary>
+        /// <param name="value">The value of a cell</param>
+        /// <returns>The value with any control chars escaped.</returns>
+        private string FormatCsvCellValue(object value)
+        {
+            if (value == null) return string.Empty;
+            string formattedValue = value.ToString();
+            // escape double quotes
+            formattedValue = formattedValue.Replace("\"", "\"\"");
+            if (formattedValue.Contains("\r") ||
+                formattedValue.Contains("\n") ||
+                formattedValue.Contains("\"") ||
+                formattedValue.Contains(",") ||
+                formattedValue.Contains(";")
+                )
+            {
+                formattedValue = string.Format("\"{0}\"", formattedValue);
+            }
+            return formattedValue;
+        }
+
+
+    }
+}
