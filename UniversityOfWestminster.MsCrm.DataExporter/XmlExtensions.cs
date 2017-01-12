6d86d2195f0eb3d5cbacdaee87a8f08a50201d63
RolfBanziger <rbaenziger@gmail.com>
2017-01-12 13:34:20 +0000

    Added missing file

---
+++ UniversityOfWestminster.MsCrm.DataExporter/XmlExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UniversityOfWestminster.MsCrm.DataExporter
+{
+    public static class XmlExtensions
+    {
+
+        public static string GetAttributeValue(this XmlNode node, string attribute)
+        {
+            var attr = node.Attributes[attribute];
+            if (attr == null) return null;
+            return attr.Value;
+        }
+    }
+}
